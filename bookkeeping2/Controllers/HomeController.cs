2b206a85b9ef41c23d828edc966975b20fcf37e5
JIM <mentha5681@gmail.com>
2017-05-17 12:17:58 +0800

    首頁新增功能完成

--- bookkeeping2/Controllers/HomeController.cs
+++ bookkeeping2/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ namespace bookkeeping2.Controllers
 {
     public class HomeController : Controller
     {
+        skilltreemodel db = new skilltreemodel();
         public ActionResult Index()
         {
             //Random ran = new Random();
@@ -20,7 +21,7 @@ namespace bookkeeping2.Controllers
             //    int randnumber = ran.Next(2);
             //    if (randnumber == 1)
             //    {
-            //        note.Type = "支出";
+            //        note.Type = "支出";                           //隨機產生資料至Myclass_model
             //    }
             //    else
             //    {
@@ -29,12 +30,78 @@ namespace bookkeeping2.Controllers
             //    note.Date = DateTime.Now.AddDays(ran.Next(3650));
             //    note.Money = ran.Next(10000);
             //    model.Add(note);
-            skilltreemodel db = new skilltreemodel();
+           
+            //var model = new List<Myclass>();
+            //Myclass ram;
+            //foreach(var i in db.AccountBook.ToList())
+            //{
+            //    ram= new Myclass();
+            //    //string[] talk;
+            //    //talk = i.Remarkkk.Split('5');
+            //    //var talk1 = "";
+            //    //foreach (var text in talk)
+            //    //{
+            //    //    talk1 += text;
+            //    //}
+            //    //ram.Remarks = (i.Remarkkk).Substring(0,10);
+            //    //if (i.Remarkkk.Length == 0)
+            //    //{
+            //    //    ram.Remarks = "邱炯達";
+            //    //}
+            //    //ram.Remarks = i.Remarkkk.Substring(0,4);
+            //    ram.Money = i.Amounttt.ToString("#,000");
+            //    ram.Date = i.Dateee.ToString("yyyy-MM-dd hh時mm分");
+            //    ram.Id = i.Id;
+            //    var 類型 = "";
+            //    if (i.Categoryyy != 0)
+            //    {
+            //        類型 = "收入";
+            //    }
+            //    else
+            //    {
+            //        類型 = "支出";
+            //    }
+            //    ram.Type = 類型.ToString();
+            //    ram.Remarks =(i.Remarkkk.Length>3)? i.Remarkkk.Substring(0,3):"我是地雷";
+            //    model.Add(ram);
+            //    }
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "Id,Categoryyy,Amounttt,Dateee,Remarkkk")] AccountBook accountBook)
+        {
+            if (ModelState.IsValid)
+            {
+                accountBook.Id = Guid.NewGuid();
+                db.AccountBook.Add(accountBook);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(accountBook);
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Ans()
+        {
             var model = new List<Myclass>();
             Myclass ram;
-            foreach(var i in db.AccountBook.ToList())
+            foreach (var i in db.AccountBook.ToList())
             {
-                ram= new Myclass();
+                ram = new Myclass();
                 //string[] talk;
                 //talk = i.Remarkkk.Split('5');
                 //var talk1 = "";
@@ -49,7 +116,7 @@ namespace bookkeeping2.Controllers
                 //}
                 //ram.Remarks = i.Remarkkk.Substring(0,4);
                 ram.Money = i.Amounttt.ToString("#,000");
-                ram.Date = i.Dateee.ToString("yyyy-MM-dd");
+                ram.Date = i.Dateee.ToString("yyyy-MM-dd hh時mm分");
                 ram.Id = i.Id;
                 var 類型 = "";
                 if (i.Categoryyy != 0)
@@ -61,24 +128,10 @@ namespace bookkeeping2.Controllers
                     類型 = "支出";
                 }
                 ram.Type = 類型.ToString();
-                ram.Remarks =(i.Remarkkk.Length>3)? i.Remarkkk.Substring(0,3):"我是地雷";
+                ram.Remarks = (i.Remarkkk.Length > 3) ? i.Remarkkk.Substring(0, 3) : "我是地雷";
                 model.Add(ram);
-                }
+            }
             return View(model);
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
         }
     }
 }
